9952d8be21e5ef8d361d899e2eef99faafb97beb
Isuru S. Aththanayake <118103842+isuru963@users.noreply.github.com>
2024-12-23 11:22:34 +0530

    6

---
+++ Test/strings/Program.cs
@@ -0,0 +1,19 @@
+﻿/*escape characters
+\n - new line
+\t tab space
+*/
+
+/*
+Console.Write("The output file is: ");
+Console.WriteLine("C:\\windows\\ubisoft\\farcry 7");
+Console.WriteLine("Isuru said \"Sri Lanka is my Country\"");
+Console.WriteLine("Isuru said \"Thilini\"");
+Console.WriteLine(@"C:\windows\games\gta6");
+*/
+
+string firstname = "Isuru";
+string greeting = "Welcome";
+
+string message = firstname + greeting + "Hello, World!";
+
+Console.WriteLine(message);
