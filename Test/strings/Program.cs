902ccf6f655f4e475a1c11a22ad7f25143c1b745
Isuru S. Aththanayake <118103842+isuru963@users.noreply.github.com>
2024-12-24 11:43:06 +0530

    7

--- Test/strings/Program.cs
+++ Test/strings/Program.cs
@@ -9,11 +9,20 @@ Console.WriteLine("C:\\windows\\ubisoft\\farcry 7");
 Console.WriteLine("Isuru said \"Sri Lanka is my Country\"");
 Console.WriteLine("Isuru said \"Thilini\"");
 Console.WriteLine(@"C:\windows\games\gta6");
-*/
+
+-------------------------------------------------------------------------------------------------
 
 string firstname = " Isuru ";
 string greeting = " Welcome ";
-
 string message = firstname + greeting + " Hello, World! ";
-
 Console.WriteLine(message);
+
+*/
+
+//String Interpolation
+
+string name = "Isuru";
+string country = "Sri Lanka";
+
+Console.WriteLine($"My name is {name}");
+
